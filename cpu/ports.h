ddce26aa76ffec265d170f58ced9d1ac214e66aa
pr-codermcun <oa2300743@gmail.com>
2026-03-19 01:35:34 +0300

    Create ports.h

---
+++ cpu/ports.h
@@ -0,0 +1,35 @@
+#ifndef WINTAKOS_PORTS_H
+#define WINTAKOS_PORTS_H
+
+#include "../include/types.h"
+
+static inline uint8_t inb(uint16_t port)
+{
+    uint8_t result;
+    __asm__ volatile("inb %1, %0" : "=a"(result) : "Nd"(port));
+    return result;
+}
+
+static inline void outb(uint16_t port, uint8_t val)
+{
+    __asm__ volatile("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline uint16_t inw(uint16_t port)
+{
+    uint16_t result;
+    __asm__ volatile("inw %1, %0" : "=a"(result) : "Nd"(port));
+    return result;
+}
+
+static inline void outw(uint16_t port, uint16_t val)
+{
+    __asm__ volatile("outw %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline void io_wait(void)
+{
+    outb(0x80, 0);
+}
+
+#endif
