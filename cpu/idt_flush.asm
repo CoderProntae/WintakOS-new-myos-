1895919515f44412e272c706607e36e510488290
pr-codermcun <oa2300743@gmail.com>
2026-03-19 01:37:33 +0300

    Update idt_flush.asm

--- cpu/idt_flush.asm
+++ cpu/idt_flush.asm
@@ -1,12 +1,6 @@
-;=============================================================================
-; WintakOS - idt_flush.asm
-; LIDT komutu ile IDT'yi CPU'ya yükler
-;=============================================================================
-
-[bits 32]
-[global idt_flush]
+global idt_flush
 
 idt_flush:
-    mov eax, [esp + 4]     ; Parametre: idt_ptr yapısının adresi
-    lidt [eax]             ; IDT'yi yükle
+    mov eax, [esp + 4]
+    lidt [eax]
     ret
