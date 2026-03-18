e56863cc56c58d84608d904b7c933c58b7c513f1
pr-codermcun <oa2300743@gmail.com>
2026-03-19 01:38:33 +0300

    Update pit.h

--- cpu/pit.h
+++ cpu/pit.h
@@ -3,8 +3,9 @@
 
 #include "../include/types.h"
 
-#define PIT_FREQUENCY 1000
+#define PIT_FREQUENCY  100
 
-void pit_init(uint32_t frequency);
+void     pit_init(uint32_t frequency);
+uint32_t pit_get_ticks(void);
 
 #endif
