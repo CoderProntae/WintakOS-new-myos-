cf609b30c44b6def4d221f90635cfe0c5c292e51
pr-codermcun <oa2300743@gmail.com>
2026-03-19 22:57:51 +0300

    Update terminal.c

--- apps/terminal.c
+++ apps/terminal.c
@@ -21,37 +21,31 @@ static void draw_char_at(uint32_t px, uint32_t py, uint8_t c, uint32_t fg, uint3
 {
     framebuffer_t* fb = fb_get_info();
     if (px >= fb->width - 8 || py >= fb->height - 16) return;
-    const uint8_t* glyph = font8x16_data[c < 128 ? c : 0];
+    const uint8_t* glyph = font8x16_data[c];
     for (uint32_t y = 0; y < 16; y++) {
         uint8_t line = glyph[y];
-        for (uint32_t x = 0; x < 8; x++) {
+        for (uint32_t x = 0; x < 8; x++)
             fb_put_pixel(px + x, py + y, (line & (0x80 >> x)) ? fg : bg);
-        }
     }
 }
 
 static void term_draw(window_t* win)
 {
     terminal_t* term = NULL;
-    for (uint32_t i = 0; i < term_count; i++) {
+    for (uint32_t i = 0; i < term_count; i++)
         if (terminals[i].win == win) { term = &terminals[i]; break; }
-    }
     if (!term) return;
 
-    int32_t base_x = win->x + 4;
-    int32_t base_y = win->y + 4;
-    if (base_x < 0 || base_y < 0) return;
+    int32_t bx = win->x + 4, by = win->y + 4;
+    if (bx < 0 || by < 0) return;
+    uint32_t px0 = (uint32_t)bx, py0 = (uint32_t)by;
 
-    uint32_t px0 = (uint32_t)base_x;
-    uint32_t py0 = (uint32_t)base_y;
     uint32_t visible = TERM_ROWS - 1;
-    uint32_t start = 0;
-    if (term->buf_count > visible) start = term->buf_count - visible;
+    uint32_t start = term->buf_count > visible ? term->buf_count - visible : 0;
 
     for (uint32_t i = 0; i < visible && (start + i) < term->buf_count; i++) {
         const char* line = term->buffer[(start + i) % TERM_BUF_LINES];
-        uint32_t px = px0;
-        uint32_t py = py0 + i * 16;
+        uint32_t px = px0, py = py0 + i * 16;
         for (uint32_t j = 0; line[j] && j < TERM_COLS; j++) {
             draw_char_at(px, py, (uint8_t)line[j], term->fg_color, term->bg_color);
             px += 8;
@@ -61,22 +55,35 @@ static void term_draw(window_t* win)
     uint32_t cmd_y = py0 + visible * 16;
     draw_char_at(px0, cmd_y, '>', RGB(100, 255, 100), term->bg_color);
     draw_char_at(px0 + 8, cmd_y, ' ', term->fg_color, term->bg_color);
+
     uint32_t cpx = px0 + 16;
     for (uint32_t i = 0; i < term->cmd_len && i < TERM_COLS - 3; i++) {
-        draw_char_at(cpx, cmd_y, (uint8_t)term->cmd[i], term->fg_color, term->bg_color);
+        uint32_t char_color = term->fg_color;
+        if (i == term->cmd_cursor) {
+            /* Imleç altindaki karakter ters renk */
+            draw_char_at(cpx, cmd_y, (uint8_t)term->cmd[i], term->bg_color, RGB(100, 255, 100));
+        } else {
+            draw_char_at(cpx, cmd_y, (uint8_t)term->cmd[i], char_color, term->bg_color);
+        }
         cpx += 8;
     }
-    uint32_t blink = (pit_get_ticks() / 25) % 2;
-    draw_char_at(cpx, cmd_y, (blink && term->active) ? '_' : ' ',
-                 RGB(100, 255, 100), term->bg_color);
+
+    /* Imleç sondaysa yanip sonen blok */
+    if (term->cmd_cursor >= term->cmd_len) {
+        uint32_t cursor_x = px0 + 16 + term->cmd_cursor * 8;
+        uint32_t blink = (pit_get_ticks() / 25) % 2;
+        if (blink && term->active)
+            draw_char_at(cursor_x, cmd_y, '_', RGB(100, 255, 100), term->bg_color);
+        else
+            draw_char_at(cursor_x, cmd_y, ' ', term->fg_color, term->bg_color);
+    }
 }
 
 static void term_add_line(terminal_t* term, const char* line)
 {
     uint32_t idx = term->buf_count % TERM_BUF_LINES;
     uint32_t i;
-    for (i = 0; i < TERM_COLS && line[i]; i++)
-        term->buffer[idx][i] = line[i];
+    for (i = 0; i < TERM_COLS && line[i]; i++) term->buffer[idx][i] = line[i];
     term->buffer[idx][i] = '\0';
     term->buf_count++;
 }
@@ -87,13 +94,9 @@ void terminal_print(terminal_t* term, const char* str)
     uint32_t pos = 0;
     while (*str) {
         if (*str == '\n' || pos >= TERM_COLS) {
-            line[pos] = '\0';
-            term_add_line(term, line);
-            pos = 0;
+            line[pos] = '\0'; term_add_line(term, line); pos = 0;
             if (*str == '\n') str++;
-        } else {
-            line[pos++] = *str++;
-        }
+        } else { line[pos++] = *str++; }
     }
     if (pos > 0) { line[pos] = '\0'; term_add_line(term, line); }
     wm_set_dirty();
@@ -119,7 +122,6 @@ static bool str_starts(const char* str, const char* prefix)
     return true;
 }
 
-/* Sayi yazdirma yardimcisi */
 static void uint_to_str(uint32_t val, char* buf, uint32_t max)
 {
     if (val == 0) { buf[0] = '0'; buf[1] = 0; return; }
@@ -135,29 +137,26 @@ static void term_execute(terminal_t* term)
     char pl[TERM_COLS + 1];
     pl[0] = '>'; pl[1] = ' ';
     uint32_t i;
-    for (i = 0; i < term->cmd_len && i < TERM_COLS - 2; i++)
-        pl[i + 2] = term->cmd[i];
-    pl[i + 2] = '\0';
+    for (i = 0; i < term->cmd_len && i < TERM_COLS - 2; i++) pl[i+2] = term->cmd[i];
+    pl[i+2] = '\0';
     term_add_line(term, pl);
 
-    if (term->cmd_len == 0) {
-        /* bos */
-    }
+    if (term->cmd_len == 0) { /* bos */ }
     else if (str_eq(term->cmd, "help")) {
         terminal_print_color(term, "Komutlar:", RGB(100, 200, 255));
-        terminal_print(term, "  help       Yardim");
-        terminal_print(term, "  clear      Ekrani temizle");
+        terminal_print(term, "  help       Yard\x01m");
+        terminal_print(term, "  clear      Ekran\x01 temizle");
         terminal_print(term, "  sysinfo    Sistem bilgileri");
-        terminal_print(term, "  uptime     Calisma suresi");
+        terminal_print(term, "  uptime     \x10" "al\x01\x03ma s\x07resi");
         terminal_print(term, "  time       Saat ve tarih");
         terminal_print(term, "  mem        Bellek durumu");
         terminal_print(term, "  ls         Dosya listesi");
         terminal_print(term, "  cat <ad>   Dosya oku");
-        terminal_print(term, "  touch <ad> Dosya olustur");
+        terminal_print(term, "  touch <ad> Dosya olu\x03tur");
         terminal_print(term, "  rm <ad>    Dosya sil");
-        terminal_print(term, "  write <ad> <icerik>");
-        terminal_print(term, "  echo <..>  Metin yazdir");
-        terminal_print(term, "  ver        Surum bilgisi");
+        terminal_print(term, "  write <ad> <i\x0Ferik>");
+        terminal_print(term, "  echo <..>  Metin yazd\x01r");
+        terminal_print(term, "  ver        S\x07r\x07m bilgisi");
     }
     else if (str_eq(term->cmd, "clear")) {
         term->buf_count = 0;
@@ -165,19 +164,18 @@ static void term_execute(terminal_t* term)
     }
     else if (str_eq(term->cmd, "sysinfo")) {
         terminal_print_color(term, "=== Sistem ===", RGB(255, 200, 100));
-        terminal_print(term, "OS:    WintakOS v0.7.0");
+        terminal_print(term, "OS:    WintakOS v0.9.0");
         terminal_print(term, "CPU:   i386 Protected");
-        char buf[40];
-        char nbuf[12];
+        char buf[40]; char nbuf[12];
         uint_to_str(pmm_get_total_mb(), nbuf, 12);
         uint32_t p = 0;
         const char* pf = "RAM:   ";
         while (*pf) buf[p++] = *pf++;
         for (uint32_t j = 0; nbuf[j]; j++) buf[p++] = nbuf[j];
-        buf[p++] = ' '; buf[p++] = 'M'; buf[p++] = 'B'; buf[p] = 0;
+        buf[p++]=' '; buf[p++]='M'; buf[p++]='B'; buf[p]=0;
         terminal_print(term, buf);
         terminal_print(term, "Video: 800x600x32 VESA");
-        terminal_print(term, "Klavye: Turkce Q");
+        terminal_print(term, "Klavye: T\x07rk\x0Fe Q");
     }
     else if (str_eq(term->cmd, "uptime")) {
         uint32_t sec = pit_get_ticks() / PIT_FREQUENCY;
@@ -202,31 +200,23 @@ static void term_execute(terminal_t* term)
     else if (str_eq(term->cmd, "mem")) {
         char buf[48]; char nbuf[12];
         terminal_print_color(term, "=== Bellek ===", RGB(255, 200, 100));
-
         uint_to_str(pmm_get_total_mb(), nbuf, 12);
-        uint32_t p = 0;
-        const char* pf = "Toplam: ";
-        while (*pf) buf[p++] = *pf++;
-        for (uint32_t j = 0; nbuf[j]; j++) buf[p++] = nbuf[j];
-        buf[p++]=' '; buf[p++]='M'; buf[p++]='B'; buf[p]=0;
+        uint32_t p=0; const char*pf="Toplam: ";
+        while(*pf) buf[p++]=*pf++;
+        for(uint32_t j=0;nbuf[j];j++) buf[p++]=nbuf[j];
+        buf[p++]=' ';buf[p++]='M';buf[p++]='B';buf[p]=0;
         terminal_print(term, buf);
-
         uint_to_str(pmm_get_free_pages(), nbuf, 12);
-        p = 0; pf = "Bos:    ";
-        while (*pf) buf[p++] = *pf++;
-        for (uint32_t j = 0; nbuf[j]; j++) buf[p++] = nbuf[j];
-        pf = " sayfa";
-        while (*pf) buf[p++] = *pf++;
-        buf[p] = 0;
+        p=0; pf="Bo\x03:    ";
+        while(*pf) buf[p++]=*pf++;
+        for(uint32_t j=0;nbuf[j];j++) buf[p++]=nbuf[j];
+        pf=" sayfa"; while(*pf) buf[p++]=*pf++; buf[p]=0;
         terminal_print(term, buf);
-
         uint_to_str(heap_get_free(), nbuf, 12);
-        p = 0; pf = "Heap:   ";
-        while (*pf) buf[p++] = *pf++;
-        for (uint32_t j = 0; nbuf[j]; j++) buf[p++] = nbuf[j];
-        pf = " byte";
-        while (*pf) buf[p++] = *pf++;
-        buf[p] = 0;
+        p=0; pf="Heap:   ";
+        while(*pf) buf[p++]=*pf++;
+        for(uint32_t j=0;nbuf[j];j++) buf[p++]=nbuf[j];
+        pf=" byte"; while(*pf) buf[p++]=*pf++; buf[p]=0;
         terminal_print(term, buf);
     }
     else if (str_eq(term->cmd, "ls")) {
@@ -236,96 +226,116 @@ static void term_execute(terminal_t* term)
             ramfs_file_t* f = ramfs_get_file(fi);
             if (!f) continue;
             uint32_t p = 0;
-            buf[p++] = ' '; buf[p++] = ' ';
-            for (uint32_t j = 0; f->name[j] && p < 30; j++) buf[p++] = f->name[j];
-            while (p < 22) buf[p++] = ' ';
+            buf[p++]=' '; buf[p++]=' ';
+            for (uint32_t j=0; f->name[j] && p<30; j++) buf[p++]=f->name[j];
+            while (p < 22) buf[p++]=' ';
             uint_to_str(f->size, nbuf, 12);
-            for (uint32_t j = 0; nbuf[j]; j++) buf[p++] = nbuf[j];
-            buf[p++] = 'B'; buf[p] = 0;
+            for (uint32_t j=0; nbuf[j]; j++) buf[p++]=nbuf[j];
+            buf[p++]='B'; buf[p]=0;
             terminal_print(term, buf);
         }
-        uint_to_str(ramfs_file_count(), nbuf, 12);
-        uint32_t p = 0;
-        for (uint32_t j = 0; nbuf[j]; j++) buf[p++] = nbuf[j];
-        const char* sf = " dosya";
-        while (*sf) buf[p++] = *sf++;
-        buf[p] = 0;
-        terminal_print(term, buf);
     }
     else if (str_starts(term->cmd, "cat ")) {
-        const char* fname = term->cmd + 4;
-        if (!ramfs_exists(fname)) {
-            terminal_print_color(term, "Dosya bulunamadi.", RGB(255, 100, 100));
-        } else {
-            char fbuf[RAMFS_MAX_FILESIZE + 1];
-            int rd = ramfs_read(fname, fbuf, RAMFS_MAX_FILESIZE);
-            if (rd > 0) {
-                fbuf[rd] = '\0';
-                terminal_print(term, fbuf);
-            }
+        const char* fn = term->cmd + 4;
+        if (!ramfs_exists(fn)) terminal_print_color(term, "Dosya bulunamad\x01.", RGB(255,100,100));
+        else {
+            char fbuf[RAMFS_MAX_FILESIZE+1];
+            int rd = ramfs_read(fn, fbuf, RAMFS_MAX_FILESIZE);
+            if (rd > 0) { fbuf[rd]='\0'; terminal_print(term, fbuf); }
         }
     }
     else if (str_starts(term->cmd, "touch ")) {
-        const char* fname = term->cmd + 6;
-        if (ramfs_exists(fname)) {
-            terminal_print(term, "Dosya zaten var.");
-        } else {
-            int r = ramfs_create(fname, false);
-            if (r >= 0) terminal_print_color(term, "Dosya olusturuldu.", RGB(100, 255, 100));
-            else terminal_print_color(term, "Hata!", RGB(255, 100, 100));
+        const char* fn = term->cmd + 6;
+        if (ramfs_exists(fn)) terminal_print(term, "Dosya zaten var.");
+        else {
+            int r = ramfs_create(fn, false);
+            if (r >= 0) terminal_print_color(term, "Olu\x03turuldu.", RGB(100,255,100));
+            else terminal_print_color(term, "Hata!", RGB(255,100,100));
         }
     }
     else if (str_starts(term->cmd, "rm ")) {
-        const char* fname = term->cmd + 3;
-        if (ramfs_delete(fname))
-            terminal_print_color(term, "Silindi.", RGB(100, 255, 100));
-        else
-            terminal_print_color(term, "Dosya bulunamadi.", RGB(255, 100, 100));
+        if (ramfs_delete(term->cmd + 3)) terminal_print_color(term, "Silindi.", RGB(100,255,100));
+        else terminal_print_color(term, "Bulunamad\x01.", RGB(255,100,100));
     }
     else if (str_starts(term->cmd, "write ")) {
-        /* write dosya.txt icerik buraya */
         const char* rest = term->cmd + 6;
-        char fname[RAMFS_MAX_NAME];
-        uint32_t fi = 0;
-        while (*rest && *rest != ' ' && fi < RAMFS_MAX_NAME - 1)
-            fname[fi++] = *rest++;
-        fname[fi] = '\0';
+        char fn[RAMFS_MAX_NAME]; uint32_t fi = 0;
+        while (*rest && *rest != ' ' && fi < RAMFS_MAX_NAME-1) fn[fi++] = *rest++;
+        fn[fi] = '\0';
         if (*rest == ' ') rest++;
-
-        if (!ramfs_exists(fname)) ramfs_create(fname, false);
-        uint32_t len = strlen(rest);
-        ramfs_write(fname, rest, len);
-        terminal_print_color(term, "Yazildi.", RGB(100, 255, 100));
+        if (!ramfs_exists(fn)) ramfs_create(fn, false);
+        ramfs_write(fn, rest, strlen(rest));
+        terminal_print_color(term, "Yaz\x01ld\x01.", RGB(100,255,100));
     }
     else if (str_eq(term->cmd, "ver")) {
-        terminal_print_color(term, "WintakOS v0.7.0", RGB(100, 200, 255));
-        terminal_print(term, "Milestone 7 - RAMFS + Terminal");
+        terminal_print_color(term, "WintakOS v0.9.0", RGB(100, 200, 255));
+        terminal_print(term, "Milestone 9");
     }
-    else if (str_starts(term->cmd, "echo ")) {
-        terminal_print(term, term->cmd + 5);
-    }
-    else {
-        terminal_print_color(term, "Bilinmeyen komut. 'help' yazin.", RGB(255, 100, 100));
-    }
+    else if (str_starts(term->cmd, "echo ")) { terminal_print(term, term->cmd + 5); }
+    else { terminal_print_color(term, "Bilinmeyen komut. 'help' yaz\x01n.", RGB(255,100,100)); }
 
-    term->cmd_len = 0;
-    term->cmd[0] = '\0';
+    term->cmd_len = 0; term->cmd_cursor = 0; term->cmd[0] = '\0';
     wm_set_dirty();
 }
 
 void terminal_input_char(terminal_t* term, uint8_t c)
 {
     if (!term || !term->active) return;
-    if (c == '\n') { term_execute(term); }
-    else if (c == '\b') {
-        if (term->cmd_len > 0) { term->cmd[--term->cmd_len] = '\0'; wm_set_dirty(); }
-    }
-    else if (c >= 0x20 || (c >= 0x01 && c <= 0x10)) {
-        if (term->cmd_len < TERM_CMD_MAX - 1) {
-            term->cmd[term->cmd_len++] = (char)c;
+
+    if (c == '\n') { term_execute(term); return; }
+    if (c == '\b') {
+        if (term->cmd_cursor > 0) {
+            /* Imleç pozisyonundan sil */
+            for (uint32_t i = term->cmd_cursor - 1; i < term->cmd_len - 1; i++)
+                term->cmd[i] = term->cmd[i + 1];
+            term->cmd_len--;
+            term->cmd_cursor--;
             term->cmd[term->cmd_len] = '\0';
             wm_set_dirty();
         }
+        return;
+    }
+    if (c >= 0x20 || (c >= 0x01 && c <= 0x10)) {
+        if (term->cmd_len < TERM_CMD_MAX - 1) {
+            /* Imleç pozisyonuna ekle */
+            for (uint32_t i = term->cmd_len; i > term->cmd_cursor; i--)
+                term->cmd[i] = term->cmd[i - 1];
+            term->cmd[term->cmd_cursor] = (char)c;
+            term->cmd_len++;
+            term->cmd_cursor++;
+            term->cmd[term->cmd_len] = '\0';
+            wm_set_dirty();
+        }
+    }
+}
+
+void terminal_input_key(terminal_t* term, uint8_t keycode)
+{
+    if (!term || !term->active) return;
+
+    switch (keycode) {
+        case KEY_LEFT:
+            if (term->cmd_cursor > 0) { term->cmd_cursor--; wm_set_dirty(); }
+            break;
+        case KEY_RIGHT:
+            if (term->cmd_cursor < term->cmd_len) { term->cmd_cursor++; wm_set_dirty(); }
+            break;
+        case KEY_HOME:
+            term->cmd_cursor = 0; wm_set_dirty();
+            break;
+        case KEY_END:
+            term->cmd_cursor = term->cmd_len; wm_set_dirty();
+            break;
+        case KEY_DELETE:
+            if (term->cmd_cursor < term->cmd_len) {
+                for (uint32_t i = term->cmd_cursor; i < term->cmd_len - 1; i++)
+                    term->cmd[i] = term->cmd[i + 1];
+                term->cmd_len--;
+                term->cmd[term->cmd_len] = '\0';
+                wm_set_dirty();
+            }
+            break;
+        default: break;
     }
 }
 
@@ -334,16 +344,16 @@ terminal_t* terminal_create(int32_t x, int32_t y)
     if (term_count >= MAX_TERMINALS) return NULL;
     terminal_t* term = &terminals[term_count++];
     memset(term->buffer, 0, sizeof(term->buffer));
-    term->buf_count = 0; term->buf_top = 0;
-    term->cmd_len = 0; term->cmd[0] = '\0';
+    term->buf_count = 0; term->cmd_len = 0; term->cmd_cursor = 0;
+    term->cmd[0] = '\0';
     term->fg_color = RGB(200, 200, 200);
     term->bg_color = RGB(15, 15, 25);
     term->active = true;
     term->win = wm_create_window(x, y, TERM_COLS * 8 + 8, TERM_ROWS * 16 + 8,
                                   "Terminal", term->bg_color);
     if (term->win) term->win->on_draw = term_draw;
-    terminal_print_color(term, "WintakOS Terminal v0.7.0", RGB(100, 200, 255));
-    terminal_print(term, "'help' yazarak baslayin.");
+    terminal_print_color(term, "WintakOS Terminal v0.9.0", RGB(100, 200, 255));
+    terminal_print(term, "'help' yazarak ba\x03lay\x01n.");
     terminal_print(term, "");
     return term;
 }
