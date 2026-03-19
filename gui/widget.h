7297116caa44bb67ea4683ea274df9794257ea22
pr-codermcun <oa2300743@gmail.com>
2026-03-19 22:36:07 +0300

    Update widget.h

--- gui/widget.h
+++ gui/widget.h
@@ -15,4 +15,18 @@ bool widget_button_hit(window_t* win, uint32_t rx, uint32_t ry,
                        uint32_t w, uint32_t h,
                        int32_t click_rx, int32_t click_ry);
 
+/* Text input widget */
+void widget_draw_textbox(window_t* win, uint32_t rx, uint32_t ry,
+                         uint32_t w, const char* text, uint32_t cursor_pos,
+                         bool focused);
+
+/* Radio button */
+void widget_draw_radio(window_t* win, uint32_t rx, uint32_t ry,
+                       const char* label, bool selected, uint32_t color);
+
+/* Progress bar */
+void widget_draw_progress(window_t* win, uint32_t rx, uint32_t ry,
+                          uint32_t w, uint32_t h, uint32_t percent,
+                          uint32_t fg, uint32_t bg);
+
 #endif
