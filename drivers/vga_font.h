e56019f7282d5ea0dbf58573b678b45a8edf7114
pr-codermcun <oa2300743@gmail.com>
2026-03-19 16:17:50 +0300

    Update vga_font.h

--- drivers/vga_font.h
+++ drivers/vga_font.h
@@ -3,12 +3,25 @@
 
 #include "../include/types.h"
 
-#define CHAR_TR_DOTLESS_I     0x01
-#define CHAR_TR_DOTTED_I      0x02
-#define CHAR_TR_S_CEDILLA     0x03
-#define CHAR_TR_S_CEDILLA_UP  0x04
-#define CHAR_TR_G_BREVE       0x05
-#define CHAR_TR_G_BREVE_UP    0x06
+/*============================================================================
+ * Turkce Karakter Kodlari
+ *
+ * 0x08=BS, 0x09=TAB, 0x0A=LF, 0x0D=CR kullaniliyor,
+ * bu yuzden o pozisyonlardan kaciniyoruz.
+ *==========================================================================*/
+
+#define CHAR_TR_DOTLESS_I       0x01   /* ı */
+#define CHAR_TR_DOTTED_CAP_I    0x02   /* İ */
+#define CHAR_TR_S_CEDILLA       0x03   /* ş */
+#define CHAR_TR_S_CEDILLA_UP    0x04   /* Ş */
+#define CHAR_TR_G_BREVE         0x05   /* ğ */
+#define CHAR_TR_G_BREVE_UP      0x06   /* Ğ */
+#define CHAR_TR_U_UMLAUT        0x07   /* ü */
+#define CHAR_TR_U_UMLAUT_UP     0x0B   /* Ü */
+#define CHAR_TR_O_UMLAUT        0x0C   /* ö */
+#define CHAR_TR_O_UMLAUT_UP     0x0E   /* Ö */
+#define CHAR_TR_C_CEDILLA       0x0F   /* ç */
+#define CHAR_TR_C_CEDILLA_UP    0x10   /* Ç */
 
 void vga_font_install_turkish(void);
 
