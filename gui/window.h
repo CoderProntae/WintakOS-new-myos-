9ab12a108eeb0cbf16166264b98ed437fb2e10d2
pr-codermcun <oa2300743@gmail.com>
2026-03-19 17:03:03 +0300

    Create window.h

---
+++ gui/window.h
@@ -0,0 +1,36 @@
+#ifndef WINTAKOS_WINDOW_H
+#define WINTAKOS_WINDOW_H
+
+#include "../include/types.h"
+
+#define MAX_WINDOWS       16
+#define TITLEBAR_HEIGHT   24
+#define BORDER_WIDTH      2
+#define CLOSE_BTN_SIZE    16
+
+#define WIN_FLAG_VISIBLE    0x01
+#define WIN_FLAG_DRAGGING   0x02
+#define WIN_FLAG_CLOSABLE   0x04
+
+typedef struct {
+    int32_t  x, y;
+    uint32_t width, height;
+    char     title[64];
+    uint32_t bg_color;
+    uint32_t title_color;
+    uint8_t  flags;
+    uint8_t  id;
+    bool     active;
+    bool     dirty;
+} window_t;
+
+void     wm_init(void);
+window_t* wm_create_window(int32_t x, int32_t y, uint32_t w, uint32_t h,
+                            const char* title, uint32_t bg_color);
+void     wm_close_window(window_t* win);
+void     wm_draw_all(void);
+void     wm_handle_mouse(int32_t mx, int32_t my, uint8_t buttons);
+window_t* wm_get_active(void);
+uint32_t wm_get_count(void);
+
+#endif
