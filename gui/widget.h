3aa3a92202973876438ca4ef2b626a00582e235d
pr-codermcun <oa2300743@gmail.com>
2026-03-19 20:10:43 +0300

    Update widget.h

--- gui/widget.h
+++ gui/widget.h
@@ -11,4 +11,8 @@ void widget_draw_button(window_t* win, uint32_t rx, uint32_t ry,
                         uint32_t w, uint32_t h, const char* text,
                         uint32_t bg, uint32_t fg);
 
+bool widget_button_hit(window_t* win, uint32_t rx, uint32_t ry,
+                       uint32_t w, uint32_t h,
+                       int32_t click_rx, int32_t click_ry);
+
 #endif
