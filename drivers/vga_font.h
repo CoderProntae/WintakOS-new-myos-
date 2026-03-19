295f745271cdecfb32248ab02e80beca172883dd
pr-codermcun <oa2300743@gmail.com>
2026-03-19 16:07:56 +0300

    Update vga_font.h

--- drivers/vga_font.h
+++ drivers/vga_font.h
@@ -3,14 +3,12 @@
 
 #include "../include/types.h"
 
-/* Turkce ozel karakterler icin CP437 kod noktalari
- * 0x01-0x06 pozisyonlarini kullaniyoruz (normalde smiley/kart simgeleri) */
-#define CHAR_TR_DOTLESS_I     0x01   /* ı */
-#define CHAR_TR_DOTTED_I      0x02   /* İ */
-#define CHAR_TR_S_CEDILLA     0x03   /* ş */
-#define CHAR_TR_S_CEDILLA_UP  0x04   /* Ş */
-#define CHAR_TR_G_BREVE       0x05   /* ğ */
-#define CHAR_TR_G_BREVE_UP    0x06   /* Ğ */
+#define CHAR_TR_DOTLESS_I     0x01
+#define CHAR_TR_DOTTED_I      0x02
+#define CHAR_TR_S_CEDILLA     0x03
+#define CHAR_TR_S_CEDILLA_UP  0x04
+#define CHAR_TR_G_BREVE       0x05
+#define CHAR_TR_G_BREVE_UP    0x06
 
 void vga_font_install_turkish(void);
 
