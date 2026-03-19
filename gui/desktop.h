7ec3ef6df39c66114608978a236f6618d0814cb7
pr-codermcun <oa2300743@gmail.com>
2026-03-19 22:38:22 +0300

    Update desktop.h

--- gui/desktop.h
+++ gui/desktop.h
@@ -7,7 +7,7 @@
 
 void desktop_init(void);
 void desktop_update(void);
-void desktop_set_busy(bool b);
+void desktop_apply_config(void);
 bool desktop_start_menu_open(void);
 int  desktop_start_menu_click(int32_t mx, int32_t my);
 
