22dcb5c82a12e3e75586c1eefec610fda3a8f887
pr-codermcun <oa2300743@gmail.com>
2026-03-20 17:56:50 +0300

    Update version.h

--- include/version.h
+++ include/version.h
@@ -2,7 +2,7 @@
 #define WINTAKOS_VERSION_H
 
 #define WINTAKOS_VERSION     "0.1.0"
-#define WINTAKOS_MILESTONE   "Milestone 11"
+#define WINTAKOS_MILESTONE   "Milestone 12"
 #define WINTAKOS_FULL        "WintakOS v0.1.0 " WINTAKOS_MILESTONE
 
 #endif
