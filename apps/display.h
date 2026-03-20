de36c4e7f9fddd706ec3454001b8fa12ddd10c16
pr-codermcun <oa2300743@gmail.com>
2026-03-20 21:12:30 +0300

    Create display.h

---
+++ apps/display.h
@@ -0,0 +1,16 @@
+#ifndef WINTAKOS_DISPLAY_H
+#define WINTAKOS_DISPLAY_H
+
+#include "../include/types.h"
+#include "../gui/window.h"
+
+typedef struct {
+    window_t* win;
+    uint32_t  selected;
+    bool      error;
+    char      error_msg[48];
+} display_app_t;
+
+display_app_t* display_create(int32_t x, int32_t y);
+
+#endif
