4ce91bfcea61b5a2fccdd82b511fddb6b56735a5
pr-codermcun <oa2300743@gmail.com>
2026-03-20 21:22:26 +0300

    Update desktop.h

--- gui/desktop.h
+++ gui/desktop.h
@@ -17,21 +17,11 @@ bool desktop_start_menu_open(void);
 int  desktop_start_menu_click(int32_t mx, int32_t my);
 
 /* Cursor modu */
-void desktop_set_cursor(uint8_t mode);
+void    desktop_set_cursor(uint8_t mode);
 uint8_t desktop_get_cursor(void);
 
 /* Masaustu ikon */
 int desktop_icon_click(int32_t mx, int32_t my);
 int desktop_icon_dblclick(int32_t mx, int32_t my);
-
-/* Cozunurluk yonetimi */
-void     desktop_request_resolution(uint32_t w, uint32_t h);
-void     desktop_confirm_resolution(void);
-void     desktop_revert_resolution(void);
-uint32_t desktop_get_screen_w(void);
-uint32_t desktop_get_screen_h(void);
-uint32_t desktop_get_max_w(void);
-uint32_t desktop_get_max_h(void);
-bool     desktop_is_confirming(void);
 
 #endif
