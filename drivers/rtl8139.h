6dd219293e9fee2271071f014e89963daf2b32e7
pr-codermcun <oa2300743@gmail.com>
2026-03-20 18:18:02 +0300

    Update rtl8139.h

--- drivers/rtl8139.h
+++ drivers/rtl8139.h
@@ -4,13 +4,9 @@
 #include "../include/types.h"
 #include "../net/netdef.h"
 
-bool     rtl8139_init(void);
-void     rtl8139_send(const void* data, uint32_t len);
-bool     rtl8139_available(void);
+bool       rtl8139_init(void);
+void       rtl8139_send(const void* data, uint32_t len);
+bool       rtl8139_available(void);
 mac_addr_t rtl8139_get_mac(void);
-
-/* Paket alma callback */
-typedef void (*rtl8139_recv_callback_t)(const void* data, uint32_t len);
-void rtl8139_set_recv_callback(rtl8139_recv_callback_t cb);
 
 #endif
