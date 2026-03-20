20ae92b98bec18d488279a369a1827ad30425714
pr-codermcun <oa2300743@gmail.com>
2026-03-20 03:32:42 +0300

    Create version.h

---
+++ include/version.h
@@ -0,0 +1,8 @@
+#ifndef WINTAKOS_VERSION_H
+#define WINTAKOS_VERSION_H
+
+#define WINTAKOS_VERSION     "0.1.0"
+#define WINTAKOS_MILESTONE   "Milestone 11"
+#define WINTAKOS_FULL        "WintakOS v0.1.0 " WINTAKOS_MILESTONE
+
+#endif
