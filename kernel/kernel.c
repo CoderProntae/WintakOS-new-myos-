d208188ec2cc5ee2084bb6cd6d02e14408683e56
pr-codermcun <oa2300743@gmail.com>
2026-03-20 03:03:49 +0300

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -11,6 +11,7 @@
 #include "../drivers/framebuffer.h"
 #include "../drivers/fbconsole.h"
 #include "../drivers/mouse.h"
+#include "../drivers/speaker.h"
 #include "../memory/pmm.h"
 #include "../memory/heap.h"
 #include "../gui/desktop.h"
@@ -22,6 +23,7 @@
 #include "../apps/notepad.h"
 #include "../apps/sysmonitor.h"
 #include "../apps/filemanager.h"
+#include "../apps/piano.h"
 #include "../fs/ramfs.h"
 
 #define MULTIBOOT2_BOOTLOADER_MAGIC  0x36D76289
@@ -60,11 +62,13 @@ static uint32_t detect_memory_kb(void* mbi_ptr)
 
 static terminal_t* main_terminal = NULL;
 static notepad_t* main_notepad = NULL;
+static piano_t* main_piano = NULL;
 
 void kernel_main(uint32_t magic, void* mbi_ptr)
 {
     gdt_init(); idt_init(); pic_init(); isr_init();
     pit_init(PIT_FREQUENCY);
+    speaker_init();
 
     bool gfx = (magic == MULTIBOOT2_BOOTLOADER_MAGIC && fb_init(mbi_ptr));
     if (!gfx) {
@@ -85,33 +89,55 @@ void kernel_main(uint32_t magic, void* mbi_ptr)
     keyboard_init();
     __asm__ volatile("sti");
 
+    /* Baslangic sesi */
+    sound_startup();
+
     desktop_init();
     setup_run();
     desktop_apply_config();
 
     /* Uygulamalar */
-    main_terminal = terminal_create(160, 40);
-    calculator_create(10, 80);
-    main_notepad = notepad_create(500, 30);
-    sysmonitor_create(480, 260);
+    main_terminal = terminal_create(150, 20);
+    calculator_create(10, 60);
+    main_notepad = notepad_create(490, 10);
+    sysmonitor_create(490, 250);
     filemanager_create(10, 310);
+    main_piano = piano_create(200, 330);
 
     wm_set_dirty();
+
+    /* Tus birakilma zamanlayicisi (piyano icin) */
+    uint32_t piano_release_tick = 0;
+    bool piano_key_held = false;
 
     while (1) {
         uint8_t c = keyboard_getchar();
         if (c) {
+            /* Aktif pencereye yonlendir */
             window_t* aw = NULL;
             for (uint32_t i = 0; i < wm_get_count(); i++) {
                 window_t* w = wm_get_window(i);
                 if (w && w->active && (w->flags & WIN_FLAG_VISIBLE)) { aw = w; break; }
             }
-            if (aw && main_terminal && aw == main_terminal->win)
+
+            if (aw && main_piano && aw == main_piano->win) {
+                piano_input_char(main_piano, c);
+                piano_key_held = true;
+                piano_release_tick = pit_get_ticks() + 15; /* ~150ms sonra birak */
+            }
+            else if (aw && main_terminal && aw == main_terminal->win)
                 terminal_input_char(main_terminal, c);
             else if (aw && main_notepad && aw == main_notepad->win)
                 notepad_input_char(main_notepad, c);
         }
 
+        /* Piyano tus birakilma simulasyonu */
+        if (piano_key_held && pit_get_ticks() >= piano_release_tick) {
+            piano_key_release(main_piano);
+            piano_key_held = false;
+        }
+
+        /* Ok tuslari */
         key_event_t ev;
         if (keyboard_poll(&ev) && !ev.released && ev.keycode != KEY_NONE) {
             window_t* aw = NULL;
