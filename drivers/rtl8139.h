b136c12ae56cfc9074ec8215a3191e68faed6aff
pr-codermcun <oa2300743@gmail.com>
2026-03-20 17:58:21 +0300

    Update rtl8139.h

--- drivers/rtl8139.h
+++ drivers/rtl8139.h
@@ -2,26 +2,15 @@
 #define WINTAKOS_RTL8139_H
 
 #include "../include/types.h"
-
-#define ETH_MTU         1500
-#define ETH_FRAME_MAX   1518
-
-typedef struct {
-    uint8_t  mac[6];
-    uint32_t ip;
-    uint16_t io_base;
-    uint8_t  irq;
-    bool     available;
-    bool     link_up;
-} nic_info_t;
+#include "../net/netdef.h"
 
 bool     rtl8139_init(void);
 void     rtl8139_send(const void* data, uint32_t len);
-nic_info_t* rtl8139_get_info(void);
+bool     rtl8139_available(void);
+mac_addr_t rtl8139_get_mac(void);
 
-/* Paket isleme callback */
-typedef void (*packet_handler_t)(const void* data, uint32_t len);
-void rtl8139_set_handler(packet_handler_t handler);
-void rtl8139_poll(void);
+/* Paket alma callback */
+typedef void (*rtl8139_recv_callback_t)(const void* data, uint32_t len);
+void rtl8139_set_recv_callback(rtl8139_recv_callback_t cb);
 
 #endif
